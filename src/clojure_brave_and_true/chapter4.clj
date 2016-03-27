59da324c9e492c9f4226b99ffca4f83422b6291f
Dan Rumney <drumney@jellyvision.com>
2016-03-27 13:47:50 -0500

    Chapter 4

--- src/clojure_brave_and_true/chapter4.clj
+++ src/clojure_brave_and_true/chapter4.clj
@@ -1,18 +1,17 @@
-(ns clojure-brave-and-true.chapter4
-  (:gen-class))
+(ns clojure-brave-and-true.chapter4 (:gen-class))
+
 (use 'clojure.pprint)
 
 (def filename "chapter4-suspects.csv")
-(def vamp-keys [:name :glitter-index])
-(defn str->int
-  [str]
-  (Integer. str))
 
-(def conversions {:name identity
-                  :glitter-index str->int})
-(defn convert
-  [vamp-key value]
-  ((get conversions vamp-key) value))
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int [str] (Integer. str))
+
+(def conversions {:name identity, :glitter-index str->int})
+(def validators {:name string?, :glitter-index number?})
+
+(defn convert [vamp-key value] ((get conversions vamp-key) value))
 
 (defn parse
   "Convert a CSV into rows of columns"
@@ -20,54 +19,56 @@
   (map #(clojure.string/split % #",")
        (clojure.string/split string #"\n")))
 
-(defn mapify
-  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
-  [rows]
-  (map (fn [unmapped-row]
-         (reduce (fn [row-map [vamp-key value]]
-                   (assoc row-map vamp-key (convert vamp-key value)))
-                 {}
-                 (map vector vamp-keys unmapped-row)))
-       rows))
+(defn
+ mapify
+ "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+ [rows]
+ (map
+  (fn
+   [unmapped-row]
+   (reduce
+    (fn [row-map [vamp-key value]] (assoc row-map vamp-key (convert vamp-key value)))
+    {}
+    (map vector vamp-keys unmapped-row)))
+  rows))
 
 (defn glitter-filter
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
-(defn map-from-reduce
-  "An implementation of map using reduce"
-  [f coll]
-  (reduce #(conj %1 (f %2)) [] coll))
+(defn
+ map-from-reduce
+ "An implementation of map using reduce"
+ [f coll]
+ (reduce #((conj %1 (f %2 [] coll)))))
 
+(def sum #((reduce + %)))
 
+(def avg #((/ (sum %) (count %))))
 
-(def sum #(reduce + %))
-(def avg #(/ (sum %) (count %)))
-(defn stats
-  [numbers]
-  (map-from-reduce #(% numbers) [sum count avg]))
+(defn stats [numbers] (map-from-reduce #((% numbers)) [sum count avg]))
 
-(defn exercise1
-  [people]
-  (map #(:name %) people))
+(defn exercise1 [people] (map #((:name %) people)))
 
+(defn append "The answer for exercise 2" [suspects suspect] (conj suspects suspect))
 
-(defn append
-  "The answer for exercise 2"
-  [suspects suspect]
-  (conj suspects suspect))
+(defn not-nil?
+  [val] ((complement nil?) val))
 
+; validators is a map of fieldnames to validation functions
+; record is the record that needs validation
+(defn
+ validate
+ "The answer for exercise 3"
+ [validators record]
+ (reduce
+  (fn
+   [is-valid vamp-key
+    (and
+     is-valid
+     (not-nil? (get record vamp-key))
+     ((get validators vamp-key) (get record vamp-key)))])
+  true
+  vamp-keys))
 
-(defn validate
-  "The answer for exercise 3"
-  [validators record]
-  (reduce ((fn [is-valid record-field]
-            body))
-    true
-    record))
-
-
-
-(defn -main
-  [& args]
-  (clojure.pprint/pprint (stats [80 1 44 13 6])))
+(defn -main [& args] (clojure.pprint/pprint (glitter-filter 3 (mapify (parse (slurp filename))))))
