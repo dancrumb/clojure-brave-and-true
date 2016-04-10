343ea4ba1eb719f5b22ebbc5f446aca87b2d4133
Dan Rumney <drumney@jellyvision.com>
2016-04-10 09:02:02 -0500

    Chapter 7

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot clojure-brave-and-true.chapter5
+  :main ^:skip-aot clojure-brave-and-true.chapter7
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/clojure_brave_and_true/chapter7.clj
@@ -0,0 +1,5 @@
+(ns clojure-brave-and-true.chapter6 (:gen-class))
+
+(use 'clojure.pprint)
+
+(defn -main [& args] (clojure.pprint/pprint '(nil)))
