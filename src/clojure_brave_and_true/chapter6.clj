653816dda8d3ed2e6776285d0aadedab78c8edfb
Dan Rumney <drumney@jellyvision.com>
2016-04-10 09:01:46 -0500

    Chapter 6

---
+++ src/clojure_brave_and_true/chapter6.clj
@@ -0,0 +1,7 @@
+(ns clojure-brave-and-true.chapter6 (:gen-class))
+
+(use 'clojure.pprint)
+
+; actually nothing in here, since this chapter is just about namespaces with no exercises
+
+(defn -main [& args] (clojure.pprint/pprint '(nil)))
